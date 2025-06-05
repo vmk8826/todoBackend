dff982ebbc275a0ca1c68869715f713ae3a3f5d1
vishnu bharadwaj <vmishra@alpha-sense.com>
2025-06-05 09:28:24 +0530

    fix: auth controller

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -102,14 +102,14 @@ export const verifyAuth = async (req, res) => {
   try {
     const token = req.cookies.token;
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: "No token provided" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.userId);
 
     if (!user) {
-      return res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: "Unauthorized User" });
     }
 
     res.status(200).json({ success: true, message: "Authenticated", user });
